532a4f9cab619ab55dcf5f45efe2909844c740bd
odaialazzeh <odai.alazzeh@gmail.com>
2023-03-28 19:17:58 +0200

    Add files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'public_recipes#index'
+  resources :users do
+    resources :foods, only: [:index, :show, :new, :create, :destroy]
+  end
 end
