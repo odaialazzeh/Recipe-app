8606fe0c8a5f2a3ee512132045f9b09944afed0d
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-03-28 18:03:43 +0300

    add destroy

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,5 +6,15 @@ class RecipesController < ApplicationController
 
   def new
   end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      flash[:success] = 'Post deleted successfully'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Error: Post could not be deleted'
+    end
+  end
 end
   
