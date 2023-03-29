688b60c83881ae2a4dd74b3cb6416eb1f772a8dd
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-03-29 10:58:12 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "recipes#index"
   resources :users do
     resources :foods, only: [:index, :show, :new, :create, :destroy]
-  root "recipes#index"
+  end
   delete '/recipes/:id', to: 'recipes#destroy', as: 'recipe'
   resources :recipes, only: [:index, :show, :new, :create, :destroy]
 end
