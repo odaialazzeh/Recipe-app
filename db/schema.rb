71cdd787556e15ea8542b26b78c540f0df0ab198
odaialazzeh <odai.alazzeh@gmail.com>
2023-03-30 17:10:50 +0200

    Update files

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_142621) do
   end
 
   create_table "inventory_foods", force: :cascade do |t|
-    t.integer "quantity"
+    t.decimal "quantity"
     t.integer "inventory_id", null: false
     t.integer "food_id", null: false
     t.datetime "created_at", null: false
@@ -72,13 +72,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_142621) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
     t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
